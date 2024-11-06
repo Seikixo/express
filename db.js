06f84ebab239876701b819f451288e3fef105aa1
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-06 16:06:48 +0800

    Setup

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    host: '127.0.0.1',
-    port: 3306,
+    host: 'db',
+    port: 5432,
     user: 'root',
     password: 'root',
     database: 'express'
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('./db');
-const port = 1337;
+const port = 3000;
 
 const app = express();
 app.use(express.json());
@@ -8,9 +8,10 @@ app.use(express.json());
 app.get('/', async(req, res) => {
     try {
         await pool.query('SELECT * FROM schools');
-        res.status(200).send(data.row);
+        res.status(200).send(data.rows);
     } catch (err) {
-        
+        console.log(err);
+        res.sendStatus(500);
     }
 })
 
